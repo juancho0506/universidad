57f091b6423e8aa3fd9f90411cdf1e6cf8ca1a0a
Rodrigo Torres <juanro.toes@gmail.com>
2014-09-02 23:14:30 -0500

    Commit inicial.

---
+++ ProyectoAutomatas2014/src/co/edu/poli/automatas/proyecto/ProyectoMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package co.edu.poli.automatas.proyecto;
+
+/**
+ * @author Rodrigo
+ *
+ */
+public class ProyectoMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
